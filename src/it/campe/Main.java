5210a3d2536ff9560a27abd73ec2155e0d10badd
Ksenx73 <hello@itcampe.com>
2021-10-14 17:05:26 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,16 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String title = "Senior Lead Principal Software Engineer Data Architect";
+        String degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
+        String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
+
+        //напишите тут ваш код
+        System.out.println("RESUME");
+        System.out.println("TITLE: " + title);
+        System.out.println("DEGREE: " + degree);
+        System.out.println("CAREER: " + career);
+    }
+}
